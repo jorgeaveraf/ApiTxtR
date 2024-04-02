c6eb278aca3a14c42402df9ab9347d7cc9640347
jorgeaveraf <jorgeaveraf@gmail.com>
2024-04-02 16:12:38 +0000

    Accidentaly changes in master

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const server = require('./server');
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);

7157d6d85d1c6564f3dbced1d5dfbb931ad212c5
jorgeaveraf <jorgeaveraf@gmail.com>
2024-04-04 02:58:27 +0000

    Configured for public ip and dockerization

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const server = require('./server');
 const port = process.env.PORT || 8080;
 
+
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
